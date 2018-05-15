1d8a8c63963c9d6a3e4a8fc21c73622a15e3e2c2
FeKozma <felix.kozma@gmail.com>
2018-05-15 15:04:47 +0200

    addScoreStrike -pass

--- src/Bowling.java
+++ src/Bowling.java
@@ -1,10 +1,24 @@
 
 public class Bowling {
 	int score = 0;
-	
+	boolean strike =false;
 	public void addScore(int first, int second)
 	{
-		score = score + first + second;
+		score += first + second;
+		//if previus was a strike
+		if(strike)
+		{
+			score += first + second;
+		}
+		//if this is a strike remember that
+		if(first == 10)
+		{
+			strike = true;
+		}
+		else
+		{
+			strike = false;
+		}
 	}
 
 	public int getScore() {
