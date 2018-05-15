9e3053f85a8cf13191ef5131222fc9033c0383b9
FeKozma <felix.kozma@gmail.com>
2018-05-15 18:11:20 +0200

    added comments

--- src/Bowling.java
+++ src/Bowling.java
@@ -6,22 +6,26 @@ public class Bowling {
 	public void addScore(int first, int second)
 	{
 		score += first + second;
+		
 		//if previus was a strike
 		if(strike)
 		{
 			score += first + second;
 			strike = false;
 		}
+		//if previus was a spare
 		else if(spare)
 		{
 			score += first;
 			spare = false;
 		}
+		
 		//if this is a strike remember that
 		if(first == 10)
 		{
 			strike = true;
 		}
+		//if it was a spare remember that
 		else if(first + second == 10)
 		{
 			spare = true;
