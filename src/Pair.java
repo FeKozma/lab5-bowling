faf5964d830acad380e1484990002eedf94a2cf8
Jonathan Ström <Livila@users.noreply.github.com>
2018-05-22 19:59:23 +0200

    Pair.java: Fixed formatting.

--- src/Pair.java
+++ src/Pair.java
@@ -1,15 +1,16 @@
-
 public class Pair {
-
 	int x;
 	int y;
+
 	public Pair(int x, int y) {
 		this.x = x;	
 		this.y = y;
-}
+	}
+
 	public int getX() {
 		return x;
 	}
+
 	public int getY() {
 		return y;
 	}
