7d0110f68316765e3e928e0a879cdc6faed60440
Jonathan Ström <Livila@users.noreply.github.com>
2018-05-22 19:55:22 +0200

    Bowling.java: Fixed formatting.

--- src/Bowling.java
+++ src/Bowling.java
@@ -1,60 +1,48 @@
-
 public class Bowling {
 	int score = 0;
-	int strike =0;
+	int strike = 0;
 	int round = 0;
 	boolean spare = false;
-	public void addScore(int first, int second)
-	{
-		if(round != 10)
-		{
-		score += first + second;
-		round++;
+	
+	public void addScore(int first, int second) {
+		if (round != 10) {
+			score += first + second;
+			round++;
 		}
 		
-		//if previus was a strike
-		if(strike > 0)
-		{
+		//If previous was a strike.
+		if (strike > 0) {
 			score += first + second;
 			
-
-			
-			if(strike >= 2)
-			{
+			if (strike >= 2) {
 				score += first;
 				strike = 0;
 			}
 			
-			//if this also is a strike remember that there wher 2 strikes in a row
-			if(first == 10)
-			{
+			// If this also is a strike, remember that there where 2 strikes in a row.
+			if (first == 10) {
 				strike = 1;
 			}
-
 		}
-		//if previus was a spare
-		else if(spare)
-		{
+		
+		// If previous was a spare.
+		else if (spare) {
 			score += first;
 			spare = false;
 		}
 		
-		//if this is a strike remember that
-		if(first == 10)
-		{
+		// If this is a strike remember that.
+		if (first == 10) {
 			strike += 1;
 		}
-		//if it was a spare remember that
-		else if(first + second == 10)
-		{
+		// If it was a spare remember that.
+		else if (first + second == 10) {
 			spare = true;
 			strike = 0;
 		}
-		else
-		{
+		else {
 			strike = 0;
 		}
-
 	}
 
 	public int getScore() {
